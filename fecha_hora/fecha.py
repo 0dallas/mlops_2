3d403e212ce2af4e293a9ebd39faa6b24639c6ef
0dallas <wrfigueroam@gmail.com>
2024-07-20 00:41:18 -0500

    imprimir_ubicacion_fin

--- fecha_hora/fecha.py
+++ fecha_hora/fecha.py
@@ -2,7 +2,9 @@ from datetime import datetime
 import os
 
 path = os.path.dirname(__file__)
+print(f'UBICACION ACTUAL: {path}')
 actual = datetime.now().strftime("Fecha actual: %Y-%m-%d %H:%M:%S")
 
 with open(os.path.join(path, 'fecha_texto.txt'),'w') as file:
     file.write(actual) 
+print("ARCHIVO GENERADO")

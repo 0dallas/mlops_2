b826d925f911f47ecf839b3c52b083586903ac48
0dallas <wrfigueroam@gmail.com>
2024-07-20 00:39:25 -0500

    archivo_ubi_actual

--- fecha_hora/fecha.py
+++ fecha_hora/fecha.py
@@ -1,5 +1,8 @@
 from datetime import datetime
+import os
+
+path = os.path.dirname(__file__)
 actual = datetime.now().strftime("Fecha actual: %Y-%m-%d %H:%M:%S")
 
-with open('fecha_texto.txt','w') as file:
-    file.write(actual)
+with open(os.path.join(path, 'fecha_texto.txt'),'w') as file:
+    file.write(actual) 

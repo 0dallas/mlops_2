b08c36dae1f1e8378f1d78d6a8901be816bb783d
0dallas <wrfigueroam@gmail.com>
2024-07-22 18:30:08 -0500

    prueba clave

--- fecha_hora/fecha.py
+++ fecha_hora/fecha.py
@@ -1,9 +1,14 @@
 from datetime import datetime
 import os
+from dotenv import load_dotenv
+
+load_dotenv('.env.local')
+key = os.getenv("CLAVE_PRIVADA")
 
 path = os.path.dirname(__file__)
 print(f'UBICACION ACTUAL: {path}')
-actual = datetime.now().strftime("Fecha: %Y-%m-%d %H:%M:%S")
+actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+actual = f"{key} dice: {actual}"
 
 with open(os.path.join(path, 'fecha_texto.txt'),'w') as file:
     file.write(actual) 

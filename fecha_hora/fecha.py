bd0a517f2e937af530d7891da22080f18e5648e0
0dallas <wrfigueroam@gmail.com>
2024-07-22 15:31:33 -0500

    prueba gitaea

--- fecha_hora/fecha.py
+++ fecha_hora/fecha.py
@@ -3,7 +3,7 @@ import os
 
 path = os.path.dirname(__file__)
 print(f'UBICACION ACTUAL: {path}')
-actual = datetime.now().strftime("Fecha actual: %Y-%m-%d %H:%M:%S")
+actual = datetime.now().strftime("Fecha: %Y-%m-%d %H:%M:%S")
 
 with open(os.path.join(path, 'fecha_texto.txt'),'w') as file:
     file.write(actual) 
